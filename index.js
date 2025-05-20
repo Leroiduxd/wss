8f8f2af23f3f6bfe3f9d0bf6943084308442f9e1
Leroiduxd <89317997+Leroiduxd@users.noreply.github.com>
2025-05-20 18:53:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,7 @@ const BASE_URL = "https://prod-kline-rest.supra.com";
 // Liste de toutes les paires à surveiller
 const PAIRS = [
   "xau_usd","xag_usd","xpd_usd","xpt_usd","xg_usd","eur_usd","usd_jpy","gbp_usd","eur_gbp","usd_krw","usd_hkd","usd_inr",
-  "usd_cny","usd_sgd","usd_thb","aud_usd","usd_cad","usd_chf","nzd_usd","usd_vnd","usd_php","usd_uah","usd_pkr","usd_brl",
-  "usd_rub","usd_idr","usd_try","usd_ngn","usd_ars","eur_aud","gbp_jpy","chf_jpy","eur_chf","aud_jpy","gbp_cad","nzd_jpy",
-  "tsla_usd","msft_usd","nvda_usd","goog_usd","aapl_usd","amzn_usd","meta_usd","nflx_usd","pypl_usd","intc_usd","coin_usd",
-  "gme_usd","amd_usd","dis_usd","brk.a_usd","baba_usd","xom_usd","tmo_usd","unh_usd","lly_usd","hd_usd","ttd_usd","crm_usd",
-  "qcom_usd","pfe_usd","abnb_usd","shop_usd","jd_usd","cvx_usd","jpm_usd","mu_usd","snap_usd","uber_usd","zm_usd","nike_usd",
-  "jnj_usd","pg_usd","cost_usd","orcle_usd","mstr_usd","spy_usd","ibit_usd","ethe_usd","etha_usd","ethv_usd","feth_usd",
-  "ethw_usd","fbtc_usd","gbtc_usd","arkb_usd","bitb_usd","v_usd","ma_usd","wmt_usd","bac_usd","abbv_usd","wfc_usd","csco_usd",
-  "mrk_usd","ko_usd","now_usd","acn_usd","abt_usd","ge_usd","lin_usd","isrg_usd","ibm_usd","pep_usd","mcd_usd","gs_usd",
-  "pm_usd","cat_usd","adbe_usd","axp_usd","ms_usd","txn_usd","intu_usd","trtx_usd","vz_usd","spgi_usd","pltr_usd","dhr_usd"
+  "usd_cny","usd_sgd","usd_thb","aud_usd","usd_cad","usd_chf","nzd_usd"
 ];
 
 // Serveur WebSocket
